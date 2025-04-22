34f5b97414df436dafbbd6ae563c2d87e37b56ae
Ilya Eryomenko <ilya@eryomenko.com>
2025-04-22 22:12:10 +0500

    feat: implement video upload handler with S3 integration and JWT validation
    Signed-off-by: Ilya Eryomenko <ilya@eryomenko.com>

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,106 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	const maxMemory = 10 << 30
+	r.Body = http.MaxBytesReader(w, r.Body, maxMemory)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	fmt.Println("uploading video", videoID, "by user", userID)
+
+	videoInfo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't get video meta info from db", err)
+		return
+	}
+
+	if videoInfo.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You don't have access to update this video", nil)
+		return
+	}
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't get extension from Content-Type header", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusInternalServerError, "Forbidden upload type (only mp4 allowed)", nil)
+		return
+	}
+	extensions, err := mime.ExtensionsByType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't get extension from Content-Type header", err)
+		return
+	}
+	ext := extensions[0]
+
+	randomBytes := make([]byte, 32)
+	rand.Read(randomBytes)
+	newVideoName := make([]byte, base64.RawURLEncoding.EncodedLen(len(randomBytes)))
+	base64.RawURLEncoding.Encode(newVideoName, randomBytes)
+	videoName := fmt.Sprint(string(newVideoName), ext)
+	tmpFile, err := os.CreateTemp("", videoName)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't create temp temp dir/file", err)
+		return
+	}
+	defer os.Remove(videoName)
+	defer tmpFile.Close()
+
+	io.Copy(tmpFile, file)
+	tmpFile.Seek(0, io.SeekStart)
+
+	cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &videoName,
+		Body:        tmpFile,
+		ContentType: &contentType,
+	})
+
+	videoURL := fmt.Sprintf("https://%v.s3.%v.amazonaws.com/%v", cfg.s3Bucket, cfg.s3Region, videoName)
+	videoInfo.VideoURL = &videoURL
+	cfg.db.UpdateVideo(videoInfo)
+
+	respondWithJSON(w, http.StatusOK, videoInfo)
+}
